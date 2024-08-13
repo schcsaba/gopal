00f3a97daa7939f5e8dc0d03a8967b067341c03a
Csaba Schnitchen <schcsaba@gmail.com>
2024-08-13 23:27:23 +0200

    feat: start project

---
+++ src/app.rs
@@ -0,0 +1,60 @@
+use crate::error_template::{AppError, ErrorTemplate};
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/gopal.css"/>
+
+        // sets the document title
+        <Title text="Leptos + Tailwindcss"/>
+
+        // content for this welcome page
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! {
+                <ErrorTemplate outside_errors/>
+            }
+            .into_view()
+        }>
+            <main>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    view! {
+        <main class="container mx-auto">
+            <section class="py-20">
+                <div class="flex flex-wrap md:flex-nowrap p-5">
+                    <div class="flex flex-col p-5 items-center text-center justify-center w-full lg:w-1/2">
+                        <h2 class="text-4xl tracking-widest uppercase pb-5" data-scroll>Discover our menu</h2>
+                        <p class="text-2xl mb-12 p-5" data-scroll>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut
+                            iaculis lectus. In lobortis tortor eget venenatis ultrices. Fusce rhoncus tincidunt purus et egestas.
+                            Maecenas erat nisl, porta nec.</p>
+                        <button class="px-3 py-4">
+                            <a class="px-3 py-4 bg-black text-white font-sans uppercase tracking-widest border hover:text-black hover:bg-white hover:border-black transition duration-300"
+                            href="meniu.html">Go to menu</a>
+                        </button>
+                    </div>
+                </div>
+            </section>
+        </main>
+    }
+}
