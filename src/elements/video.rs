b780af69b6488e9b1be53811acc01502db3415cb
Csaba Schnitchen <schcsaba@gmail.com>
2024-08-18 10:19:12 +0200

    feat: replace video

--- src/elements/video.rs
+++ src/elements/video.rs
@@ -4,7 +4,7 @@ use leptos::*;
 pub fn Video() -> impl IntoView {
     view! {
         <video autoplay class="absolute z-10 w-full h-full object-cover" id="myVideo" playsinline loop muted poster="assets/img/sunset.jpg">
-            <source src="assets/img/video/picnic.mp4" type="video/mp4" />
+            <source src="assets/img/video/le_gopal_clip.mp4" type="video/mp4" />
         </video>
     }
 }
