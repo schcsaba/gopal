573b73c2ad206717f3693c9a04b08d1bcf909119
Csaba Schnitchen <schcsaba@gmail.com>
2024-09-02 20:03:10 +0200

    feat: move reservation iframe to the top of the page

--- src/pages/reservation.rs
+++ src/pages/reservation.rs
@@ -9,15 +9,15 @@ pub fn Reservation() -> impl IntoView {
             <Html lang="fr" />
             <section class="container mx-auto my-20">
                 <div class="text-center">
-                    <h2 class="font-sans font-bold text-4xl pb-6 uppercase tracking-wide">Sur place ou à emporter ou livraison à domicile</h2>
+                    <h2 class="font-sans font-bold text-4xl pt-6 pb-6 uppercase tracking-wide">Réservez une table</h2>
+                    <iframe class="pb-6" src="https://widget.thefork.com/60d93c62-f1bb-412e-9206-93ab3a1900cb" allow="payment *" title="TheFork" style="width: 100%; min-height: 800px; border: none; overflow: scroll;"></iframe>
+                    <h2 class="font-sans font-bold text-3xl pb-6 uppercase tracking-wide">Sur place ou à emporter ou livraison à domicile</h2>
                     <p class="text-2xl pb-4">Livraison à domicile : <span class="underline decoration-1 underline-offset-2"><a href="https://www.frerestoque.fr/shop/angers-centre-ville/gopal" target="_blank" rel="noreferrer">Cliquez ici</a></span></p>
                     <p class="text-2xl pb-4">Tél : <span class="underline decoration-1 underline-offset-2"><a href="tel:+33783654565">07 83 65 45 65</a></span></p>
                     <p class="text-2xl pb-4">Email : <span class="underline decoration-1 underline-offset-2"><a href="mailto:contact@legopal.fr">contact@legopal.fr</a></span></p>
                     <p class="text-2xl pb-6">Messenger : <span class="underline decoration-1 underline-offset-2"><a href="https://m.me/Legopaltours" target="_blank" rel="noreferrer">LeGopaltours</a></span></p>
                     <p class="text-xl pb-6">"Méthodes de paiement : Espèces · Visa · Mastercard · Ticket restaurant"</p>
                     <p class="text-2xl pb-6 font-extrabold">Tous nos plats sont faits maison.</p>
-                    <h2 class="font-sans font-bold text-4xl pt-6 pb-6 uppercase tracking-wide">Réservez une table</h2>
-                    <iframe src="https://widget.thefork.com/60d93c62-f1bb-412e-9206-93ab3a1900cb" allow="payment *" title="TheFork" style="width: 100%; min-height: 800px; border: none; overflow: scroll;"></iframe>
                 </div>
                 <div class="text-center my-10">
                     <h2 class="font-sans font-bold text-3xl pb-6 uppercase tracking-wide">"Heures d'ouverture : "</h2>
