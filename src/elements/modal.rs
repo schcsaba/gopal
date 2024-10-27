70329c9f86083e86815c2e5c435b169c818f2076
Csaba Schnitchen <schcsaba@gmail.com>
2024-10-27 21:28:40 +0100

    fix: make announcement modal smaller

--- src/elements/modal.rs
+++ src/elements/modal.rs
@@ -26,7 +26,7 @@ where
 
         <div class="fixed inset-0 z-30 w-screen overflow-y-auto">
           <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <div on:click=stop_propagation class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full lg:max-w-2xl md:max-w-2xl  sm:max-w-lg">
+            <div on:click=stop_propagation class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full lg:max-w-xl md:max-w-xl sm:max-w-lg">
               <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                   <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
